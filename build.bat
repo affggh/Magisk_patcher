ce08ae1905e703ae9fd20b1168c342841ad40048
affggh <879632264@qq.com>
2022-11-01 11:07:30 +0800

    Use nuitka replace pyinstaller pack exe

---
+++ build.bat
@@ -0,0 +1,1 @@
+python -m nuitka --standalone --onefile --follow-imports --mingw64 --enable-plugin=tk-inter --disable-console --include-data-dir=bin=bin --windows-icon-from-ico=bin/logo.ico .\Magiskpatcher.py
